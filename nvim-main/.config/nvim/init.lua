491428258bc527080ffcda892190914e8124275a
Mr-Sniff <teodboe@gmail.com>
2026-04-01 23:25:35 +0200

    nvim, hyprland, waybar and stuff

--- nvim-main/.config/nvim/init.lua
+++ nvim-main/.config/nvim/init.lua
@@ -1,6 +1,33 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("gruvbox").load()
+vim.opt.termguicolors = true
+
+-- transperency
+require("transparent").setup({
+  -- These groups are cleared by default, but we'll add the tab/buffer groups
+  extra_groups = {
+    "NormalFloat", -- Floats like Lazy, Mason, and LspInfo
+    "FloatBorder", -- Borders of floating windows
+    "TelescopeNormal",
+    "TelescopeBorder",
+    "TelescopePromptBorder",
+    "TelescopeResultsBorder",
+    "TelescopePreviewBorder",
+    "TabLine", -- The buffer tabs at the top
+    "TabLineFill",
+    "TabLineSel",
+    "SnacksNormal",
+    "SnacksNormalNC",
+    "SnacksWinBar",
+    "SnacksWinBarNC",
+    -- If you use the Snacks Picker (instead of Telescope)
+    "SnacksPickerNormal",
+    "SnacksPickerBorder",
+  },
+})
+
+--godot mcp
+-- Run immediately for the current session
 if vim.fn.filereadable(vim.fn.getcwd() .. "/project.godot") == 1 then
   local addr = "./godot.pipe"
   vim.fn.serverstart(addr)
--- nvim-main/.config/nvim/lua/plugins/theme.lua
+++ nvim-main/.config/nvim/lua/plugins/theme.lua
@@ -10,11 +10,38 @@ return {
   {
     "craftzdog/solarized-osaka.nvim",
   },
+  {
+    "rktjmp/lush.nvim",
+  },
+  {
+    "metalelf0/jellybeans-nvim",
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+  },
+  {
+    "sainnhe/edge",
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "navarasu/onedark.nvim",
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "moonfly",
     },
   },
 }
